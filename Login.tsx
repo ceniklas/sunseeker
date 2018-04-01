593f0e567b40a6938741446b9f941b5739e783c5
Niklas Fransson <niklas.fransson@hiq.se>
2018-04-02 00:57:08 +0200

    Super cool facebook firebase signin stuff

--- Login.tsx
+++ Login.tsx
@@ -1,8 +1,67 @@
 import * as React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase';
+import { Facebook } from 'expo';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDujWsYdKDZZ-zUdO_ZVzh5lf8aR9N-Jf8",
+  authDomain: "sunseeker-firebase.firebaseapp.com",
+  databaseURL: "https://sunseeker-firebase.firebaseio.com",
+  projectId: "sunseeker-firebase",
+  storageBucket: "sunseeker-firebase.appspot.com",
+  messagingSenderId: "721552557682"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+async function logIn() {
+  const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+    '2041975362707985',
+    { permissions: ['public_profile'] },
+  );
+
+  if (type === 'success') {
+    // Build Firebase credential with the Facebook access token.
+    const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+    // Sign in with credential from the Facebook user.
+    firebase.auth().signInWithCredential(credential).catch((error) => {
+      console.error(error);
+    });
+
+
+    // Get the user's name using Facebook's Graph API
+    const response = await fetch(
+      `https://graph.facebook.com/me?access_token=${token}`);
+    Alert.alert(
+      'Logged in!',
+      `Hi ${(await response.json()).name}!`,
+    );
+  }
+}
 
 export default class Login extends React.Component<{}> {
+  state = { isAuthenticated: 'darkred' }
+
+  constructor(props) {
+    super(props);
+
+    // Listen for authentication state to change.
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user != null) {
+        this.setState({ isAuthenticated: 'green' });
+      }
+    });
+  }
+
   render() {
-    return <View><Text>Login breh</Text></View>
+    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: this.state.isAuthenticated }}>
+      <TouchableOpacity onPress={() => logIn()}>
+        <Text>Login breh</Text>
+      </TouchableOpacity>
+    </View>
   }
 }
