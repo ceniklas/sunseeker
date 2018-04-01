a1c585a234bd58936578c0dfd97a9c0d08311899
Jacob Pålsson <jacpa975@student.liu.se>
2018-04-01 22:01:00 +0200

    Swag

---
+++ Login.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { Text, View } from 'react-native';
+
+export default class Login extends React.Component<{}> {
+  render() {
+    return <View><Text>Login breh</Text></View>
+  }
+}
